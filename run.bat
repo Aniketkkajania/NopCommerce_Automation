7ce266d911ae3bb99da71061b642b92c9ab689cd
Aniket Kajania <67728984+Aniketkkajania@users.noreply.github.com>
2024-11-07 16:49:59 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-pytest -v -s -p no:warnings -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+python pytest -v -s -p no:warnings -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 ::python -v -s -p no:warnings -m "regression" --html=./Reports/report.html --browser chrome
 ::python -v -s -p no:warnings -m "sanity or regression" --html=./Reports/report.html --browser chrome
 ::python -v -s -p no:warnings -m "sanity and regression" --html=./Reports/report.html --browser chrome
